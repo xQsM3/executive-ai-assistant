cb11e54572835d0e546e105bb091dbaf2546df5c
Luc Stiemer <luc.stiemer@axontic.com>
2025-03-23 14:06:52 +0100

    triage works now

--- eaia/main/nodes/triage.py
+++ eaia/main/nodes/triage.py
@@ -11,7 +11,10 @@ from eaia.schemas import (
 )
 from eaia.main.config.fewshot import get_few_shot_examples
 from eaia.main.config.config import get_config
+import logging
 
+# Set up the logger for your Langgraph application
+logger = logging.getLogger("langgraph")
 
 triage_prompt = """You are {full_name}'s executive assistant. You are a top-notch executive assistant who cares about {name} performing as well as possible.
 
@@ -44,10 +47,20 @@ Subject: {subject}
 #{triage_notify}
 
 async def triage_input(state: State, config: RunnableConfig, store: BaseStore):
-    model = config["configurable"].get("model", "gpt-4o")
+
+    logger.info("FOFOFOFOFOFOO")
+    # logger.info(input_message)
+    model = config["configurable"].get("model", "gpt-4o-mini")
+    if not model:
+        model = "gpt-4o-mini"
+
+    logger.info(f"fofofo model name from config {model}")
     llm = ChatOpenAI(model=model, temperature=0)
+    logger.info("fofofo created llm ")
     examples = await get_few_shot_examples(state["email"], store, config)
+    logger.info("fofofo exammples")
     prompt_config = get_config(config)
+    logger.info("FOFOFO creating input message")
     input_message = triage_prompt.format(
         email_thread=state["email"]["page_content"],
         author=state["email"]["from_email"],
@@ -64,6 +77,8 @@ async def triage_input(state: State, config: RunnableConfig, store: BaseStore):
     model = llm.with_structured_output(RespondTo).bind(
         tool_choice={"type": "function", "function": {"name": "RespondTo"}}
     )
+    logger.info("fofofo input message")
+    logger.info(input_message)
     response = await model.ainvoke(input_message)
     if len(state["messages"]) > 0:
         delete_messages = [RemoveMessage(id=m.id) for m in state["messages"]]
